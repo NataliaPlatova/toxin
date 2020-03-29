277e9e55da1367fb91190c8682d2bbe3db7b5a49
Natalia <n.platova@hotmail.com>
2020-03-29 21:48:14 +0700

    add dropdown jq plugin

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import * as $ from  'jquery';
 import './css/styles.css';
 import './scss/styles.scss';
+import 'item-quantity-dropdown';
+import 'item-quantity-dropdown/lib/item-quantity-dropdown.css';
 
 console.log('Hello world!');
 
