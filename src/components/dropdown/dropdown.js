4382ba22b8c72b18c365dcb3f6814d9ff5d9c72c
Natalia <n.platova@hotmail.com>
2020-04-01 21:53:49 +0700

    correcting styles for item-quantity-dropdown

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,6 +1,6 @@
-import './dropdown.scss';
 require('item-quantity-dropdown/lib/item-quantity-dropdown.min.js');
+import './dropdown.scss';
 
 $(document).ready(function() {
-    $(".iqdropdown").iqDropdown();
+    $(".dropdown").iqDropdown();
 });
