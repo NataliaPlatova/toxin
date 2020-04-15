df83ad27725981f06d88a05b7d44a186f5bb19dd
Natalia <n.platova@hotmail.com>
2020-04-15 18:18:28 +0700

    js for rate button in process

--- src/components/rate-button/rate-button.js
+++ src/components/rate-button/rate-button.js
@@ -1,1 +1,14 @@
-import './rate-button.scss'
+import './rate-button.scss';
+
+let stars = document.querySelectorAll('.rate-button__star');
+
+const colorStars = function(id) {
+  let achievedStars = document.querySelectorAll('.rate-button__star_achieved');
+  for(let achievedId=1; achievedId<=achievedStars.length; achievedId++) {
+      achievedStars[achievedId].classList.remove('rate-button__star_achieved');
+  }
+};
+
+for(let starId=1; starId<=stars.length; starId++) {
+  stars[starId].addEventListener('click', colorStars(starId));
+}
