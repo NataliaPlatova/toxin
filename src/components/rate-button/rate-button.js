23cc99901b7f1f838c646051953a61d42dd6d78e
Natalia <n.platova@hotmail.com>
2020-04-15 19:10:37 +0700

    changed js in rate button

--- src/components/rate-button/rate-button.js
+++ src/components/rate-button/rate-button.js
@@ -2,13 +2,19 @@ import './rate-button.scss';
 
 let stars = document.querySelectorAll('.rate-button__star');
 
-const colorStars = function(id) {
-  let achievedStars = document.querySelectorAll('.rate-button__star_achieved');
-  for(let achievedId=1; achievedId<=achievedStars.length; achievedId++) {
-      achievedStars[achievedId].classList.remove('rate-button__star_achieved');
-  }
+const addRateHandler = function(star, id) {
+    star.addEventListener('click', function() {
+        let achievedStars = document.querySelectorAll('.rate-button__star_achieved');
+        for(let achievedId=0; achievedId<achievedStars.length; achievedId++) {
+            achievedStars[achievedId].classList.remove('rate-button__star_achieved');
+        }
+        for(let newAchievedId=0; newAchievedId<=id; newAchievedId++) {
+            console.log(stars[newAchievedId]);
+            stars[newAchievedId].classList.add('rate-button__star_achieved');
+        }
+    });
 };
 
-for(let starId=1; starId<=stars.length; starId++) {
-  stars[starId].addEventListener('click', colorStars(starId));
+for(let starId=0; starId<stars.length; starId++) {
+  addRateHandler(stars[starId], starId);
 }
