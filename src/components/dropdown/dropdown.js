96f6649b60a05f6f38c89e74e0cea5efe474a546
Natalia <n.platova@hotmail.com>
2020-04-17 19:58:08 +0700

    changed dropdown component, add default text to dropdown without items

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -2,5 +2,18 @@ require('item-quantity-dropdown/lib/item-quantity-dropdown.min.js');
 import './dropdown.scss';
 
 $(document).ready(function() {
-    $(".dropdown").iqDropdown();
+    $(".dropdown").iqDropdown({
+        onChange: (id, count, totalItems) => {
+            console.log('hhhgg');
+            if(totalItems===0) {
+                $('.dropdown__selection').css('display', 'none');
+                $('.dropdown__default-text').css('display', 'block');
+                console.log('null');
+            } else {
+                $('.dropdown__selection').css('display', 'block');
+                $('.dropdown__default-text').css('display', 'none');
+            }
+        }
+    });
 });
+
