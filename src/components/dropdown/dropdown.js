1768d9bfaab7e44a58be5baeabe2486a8f32af2d
Natalia <n.platova@hotmail.com>
2020-04-20 14:15:33 +0700

    changed dropdown component: plural for 1, 21, 31, etc item; 2,3,4,22,23,24 etc. items; 5-20, 25-30, 35-40 etc. items

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -4,14 +4,22 @@ import './dropdown.scss';
 $(document).ready(function() {
     $(".dropdown").iqDropdown({
         onChange: (id, count, totalItems) => {
-            console.log('hhhgg');
             if(totalItems===0) {
                 $('.dropdown__selection').css('display', 'none');
                 $('.dropdown__default-text').css('display', 'block');
-                console.log('null');
             } else {
                 $('.dropdown__selection').css('display', 'block');
                 $('.dropdown__default-text').css('display', 'none');
+            }
+            let a = totalItems%10;
+            if((a>=5 || a===0) || (totalItems%100<=20 && totalItems%100>=11)){
+                let selection = $('.dropdown__selection');
+                let selectionPluralText = selection.attr('data-text-plural-2');
+                selection.html(`${totalItems} ${selectionPluralText}`);
+            } else if(a===1) {
+                let selection = $('.dropdown__selection');
+                let selectionPluralText = selection.attr('data-text-plural-3');
+                selection.html(`${totalItems} ${selectionPluralText}`);
             }
         }
     });
