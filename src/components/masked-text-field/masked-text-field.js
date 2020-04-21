82c65c437df5fc121f00c75d6cfff7c1bf5d7a46
Natalia <n.platova@hotmail.com>
2020-04-21 16:50:02 +0700

    changed placeholder in masked text-field component

--- src/components/masked-text-field/masked-text-field.js
+++ src/components/masked-text-field/masked-text-field.js
@@ -1,7 +1,7 @@
 import './masked-text-field.scss';
 var Inputmask = require('inputmask');
 
-var selector = document.getElementsByClassName("masked-input");
+var selector = document.querySelector(".masked-input");
 
-var im = new Inputmask({alias: "datetime", inputFormat: "dd/mm/yyyy"});
+var im = new Inputmask({alias: "datetime", inputFormat: "dd\.mm\.yyyy", placeholder: "ДД.ММ.ГГГГ"});
 im.mask(selector);
