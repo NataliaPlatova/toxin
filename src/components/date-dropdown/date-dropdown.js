314aa28944c04f904af409f37e4b281da49c1956
Natalia <n.platova@hotmail.com>
2020-04-22 15:58:17 +0700

    add date-dropdown component without calendar styles

--- src/components/date-dropdown/date-dropdown.js
+++ src/components/date-dropdown/date-dropdown.js
@@ -1,10 +1,12 @@
 import './date-dropdown.scss';
 require('@chenfengyuan/datepicker');
+require('@chenfengyuan/datepicker/i18n/datepicker.ru-RU')
 
 jQuery(document).ready(function($) {
     $('.date-dropdown').datepicker({
+      language: 'ru-RU',
       autoHide: true,
-      language: 'ru-RU'
+      format: 'dd\.mm\.yyyy'
     });
 });
 
