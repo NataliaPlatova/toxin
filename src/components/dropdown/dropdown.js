49ac9a49627e1193de980415fd7bee12aa70a5e2
Natalia <n.platova@hotmail.com>
2020-04-23 15:08:15 +0700

    deleted onChange option in dropdown comp.

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -2,22 +2,20 @@ require('item-quantity-dropdown/lib/item-quantity-dropdown.min.js');
 import './dropdown.scss';
 
 $(document).ready(function() {
-    $(".dropdown").iqDropdown(
-        {
-            setSelectionText(itemCount, totalItems) {
-                if (totalItems === 0) {
-                    return "Сколько гостей";
-                } else if((totalItems%10>=5 || totalItems%10===0) || (totalItems%100<=20 && totalItems%100>=11)){
-                    return `${totalItems} гостей`;
-                }else if(totalItems%10===1) {
-                    return `${totalItems} гость`;
-                }else {
-                    return `${totalItems} гостя`;
-                }
-            },
-            onChange: (id, count, totalItems) => {
-                this.setSelectionText(count, totalItems);
-            }
-        });
+  $(".dropdown").iqDropdown(
+    {
+      setSelectionText: (itemCount, totalItems)=>{
+        if (totalItems === 0) {
+          return 'Сколько гостей';
+        } else if((totalItems%10>=5 || totalItems%10===0) || (totalItems%100<=20 && totalItems%100>=11)){
+          return `${totalItems} гостей`;
+        }else if(totalItems%10===1) {
+          return `${totalItems} гость`;
+        }else{
+          return `${totalItems} гостя`;
+        }
+      }
+    }
+  );
 });
 
