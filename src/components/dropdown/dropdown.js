a302fdb0b769d40bd60e3d17daa261d318766f93
Natalia <n.platova@hotmail.com>
2020-06-02 19:15:02 +0700

    changed dropdown.js,now its possible to make 2 or more dropdowns on the same page

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -2,20 +2,25 @@ require('item-quantity-dropdown/lib/item-quantity-dropdown.min.js');
 import './dropdown.scss';
 
 $(document).ready(function() {
-  $(".dropdown").iqDropdown(
-    {
-      setSelectionText: (itemCount, totalItems)=>{
-        if (totalItems === 0) {
-          return 'Сколько гостей';
-        } else if((totalItems%10>=5 || totalItems%10===0) || (totalItems%100<=20 && totalItems%100>=11)){
-          return `${totalItems} гостей`;
-        }else if(totalItems%10===1) {
-          return `${totalItems} гость`;
-        }else{
-          return `${totalItems} гостя`;
-        }
-      }
-    }
-  );
+  const allPageDropdowns = $('.iqdropdown');
+  for (let i=0; i<allPageDropdowns.length; i++) {
+    const dropdown = $(allPageDropdowns[i]);
+    const dropdownSelection = $(dropdown.children()[0]);
+       dropdown.iqDropdown(
+           {
+             setSelectionText: (itemCount, totalItems) => {
+               if (totalItems === 0) {
+                 return `${dropdownSelection.data('placeholder')}`;
+               } else if ((totalItems % 10 >= 5 || totalItems % 10 === 0) || (totalItems % 100 <= 20 && totalItems % 100 >= 11)) {
+                 return `${totalItems} ${dropdownSelection.data('text-plural-2')}`;
+               } else if (totalItems % 10 === 1) {
+                 return `${totalItems} ${dropdownSelection.data('selection-text')}`;
+               } else {
+                 return `${totalItems} ${dropdownSelection.data('text-plural')}`;
+               }
+             }
+           }
+       );
+  }
 });
 
